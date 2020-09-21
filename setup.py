0c89798b1cd3da10578ee5156f75cc4295434feb
Alex Hadjinicolaou <a.e.hadjinicolaou@gmail.com>
2020-09-20 22:12:16 -0400

    Switched to Bootstrap-Flask

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='issues',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
-    install_requires=["flask", "pytest", "pytest-flask", "flask-bootstrap"],
+    install_requires=["flask", "pytest", "pytest-flask", "bootstrap-flask"],
 )
--- src/main/__init__.py
+++ src/main/__init__.py
@@ -9,27 +9,46 @@ bootstrap = Bootstrap()
 
 # factory function
 def create_app(config_name):
-   app = Flask(__name__, template_folder='../templates')
+   app = Flask(__name__, template_folder='../templates', static_folder='../static')
    app.config.from_object(config[config_name])
 
    bootstrap.init_app(app)
 
    @app.route('/')
    def index():
-      return '<h1>Show me the money!</h1>'
+      return render_template('dashboard.html',
+            is_active={},
+            user_data=get_user_data())
 
-   @app.route('/greeting/<name>')
-   def greeting(name):
-      return render_template('greeting.html', name=name)
+   @app.route('/projects')
+   def projects():
+      return render_template('projects.html',
+            is_active={'projects': True},
+            user_data=get_user_data())
 
-   @app.route('/fancy-greeting/<name>')
-   def fancy_greeting(name):
-      return render_template('fancy_greeting.html',
-            name=name,
-            hour=datetime.now().hour)
+   @app.route('/issues')
+   def issues():
+      return render_template('issues.html',
+            is_active={'issues': True},
+            user_data=get_user_data())
+
+   @app.route('/messages')
+   def messages():
+      return render_template('messages.html',
+            is_active={'messages': True},
+            user_data=get_user_data())
 
    @app.errorhandler(404)
-   def not_found(e):
-      return "<h1>Looks like I can't find that page...</h1>", 404
+   def page_not_found(e):
+      return render_template("404.html",
+            is_active={},
+            user_data={}), 404
+
+   def get_user_data():
+      return {
+         'user': 'ahadjinicolaou',
+         'role': 'admin',
+         'num_issues': 12,
+         'num_messages': 2}
 
    return app
